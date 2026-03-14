299229dc5c93170872074c60f28ad34d5b9fd95a
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2026-03-14 03:19:51 -0300

    feat: refine ai onboarding and category ergonomics

--- frontend/src/components/layout/NavigationIcons.tsx
+++ frontend/src/components/layout/NavigationIcons.tsx
@@ -16,7 +16,7 @@ type NavItem = {
   icon: IconName;
 };
 
-const iconClassName = "h-4 w-4";
+const iconClassName = "h-[18px] w-[18px]";
 
 export const APP_NAV_ITEMS: NavItem[] = [
   { href: "/dashboard", label: "Dashboard", icon: "dashboard" },
@@ -34,7 +34,7 @@ function IconBase(props: SVGProps<SVGSVGElement>) {
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth="1.8"
+      strokeWidth="1.85"
       viewBox="0 0 24 24"
       {...props}
     />
@@ -46,68 +46,66 @@ export function NavigationIcon({ name, className = iconClassName }: { name: Icon
     case "dashboard":
       return (
         <IconBase className={className}>
-          <path d="M4.5 7.5A3 3 0 0 1 7.5 4.5h3a3 3 0 0 1 3 3v3a3 3 0 0 1-3 3h-3a3 3 0 0 1-3-3v-3Z" />
-          <path d="M15 7.5a3 3 0 0 1 3-3h.5a2.5 2.5 0 0 1 2.5 2.5V13a2.5 2.5 0 0 1-2.5 2.5H18a3 3 0 0 1-3-3V7.5Z" />
-          <path d="M4.5 17.5a2.5 2.5 0 0 1 2.5-2.5H11a2.5 2.5 0 0 1 2.5 2.5v.5A2.5 2.5 0 0 1 11 20.5H7A2.5 2.5 0 0 1 4.5 18v-.5Z" />
-          <path d="M15 18a2.5 2.5 0 0 1 2.5-2.5h1A2.5 2.5 0 0 1 21 18a2.5 2.5 0 0 1-2.5 2.5h-1A2.5 2.5 0 0 1 15 18Z" />
+          <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3Z" />
+          <path d="M13 6.5A1.5 1.5 0 0 1 14.5 5h3A1.5 1.5 0 0 1 19 6.5v6A1.5 1.5 0 0 1 17.5 14h-3A1.5 1.5 0 0 1 13 12.5v-6Z" />
+          <path d="M5 14.5A1.5 1.5 0 0 1 6.5 13h3a1.5 1.5 0 0 1 1.5 1.5v3A1.5 1.5 0 0 1 9.5 19h-3A1.5 1.5 0 0 1 5 17.5v-3Z" />
+          <path d="M13 17a2 2 0 0 1 2-2h2" />
+          <path d="M13 17a2 2 0 0 0 2 2h2" />
         </IconBase>
       );
     case "transactions":
       return (
         <IconBase className={className}>
-          <path d="M6 7.5h8.5" />
-          <path d="m12.5 4.8 2.7 2.7-2.7 2.7" />
-          <path d="M18 16.5H9.5" />
-          <path d="m11.5 13.8-2.7 2.7 2.7 2.7" />
-          <path d="M4.5 5.5v13" />
-          <path d="M19.5 5.5v13" />
+          <path d="M6 7.5h10" />
+          <path d="m13.5 5 2.5 2.5-2.5 2.5" />
+          <path d="M18 16.5H8" />
+          <path d="m10.5 14-2.5 2.5 2.5 2.5" />
         </IconBase>
       );
     case "categories":
       return (
         <IconBase className={className}>
-          <path d="M12 4.5 5 8.6 12 12.7l7-4.1L12 4.5Z" />
-          <path d="M5 12.2 12 16.3l7-4.1" />
-          <path d="M5 15.9 12 20l7-4.1" />
+          <path d="M6 8.2 12 5l6 3.2-6 3.3-6-3.3Z" />
+          <path d="M6 12.1 12 15.4l6-3.3" />
+          <path d="M6 15.9 12 19l6-3.1" />
         </IconBase>
       );
     case "ledger":
       return (
         <IconBase className={className}>
-          <rect x="4.5" y="5" width="15" height="14" rx="3" />
-          <path d="M8 9h8" />
-          <path d="M8 13h8" />
-          <path d="M8 17h4.5" />
-          <path d="M15.7 5v14" />
+          <rect x="5" y="4.8" width="14" height="14.5" rx="2.6" />
+          <path d="M8.5 8.5h7" />
+          <path d="M8.5 12.2h7" />
+          <path d="M8.5 15.9h4" />
         </IconBase>
       );
     case "ask-ai":
       return (
         <IconBase className={className}>
-          <path d="M12 4.5c4.6 0 8.2 2.9 8.2 6.6 0 2-1 3.8-2.8 5l.3 3.4-3.1-1.6a11 11 0 0 1-2.6.3c-4.6 0-8.2-2.9-8.2-6.6S7.4 4.5 12 4.5Z" />
-          <path d="M9 10.5h6" />
-          <path d="M9.8 13.5h4.4" />
+          <path d="M12 5c4.4 0 8 2.7 8 6.2 0 1.9-1 3.6-2.7 4.8l.4 3-2.8-1.5a10.7 10.7 0 0 1-2.9.4c-4.4 0-8-2.7-8-6.2S7.6 5 12 5Z" />
+          <path d="M8.7 10.5h6.6" />
+          <path d="M10.1 13.5h3.8" />
         </IconBase>
       );
     case "lock":
       return (
         <IconBase className={className}>
-          <rect x="5.5" y="11" width="13" height="8" rx="2.2" />
-          <path d="M8.5 11V8.7a3.5 3.5 0 1 1 7 0V11" />
+          <rect x="5.5" y="10.8" width="13" height="8.2" rx="2.2" />
+          <path d="M8.5 10.8V8.7a3.5 3.5 0 1 1 7 0v2.1" />
         </IconBase>
       );
     case "collapse":
       return (
         <IconBase className={className}>
-          <path d="M6.5 5v14" />
-          <path d="m16 7.2-4 4.8 4 4.8" />
+          <path d="M7 5v14" />
+          <path d="m16 7.4-4 4.6 4 4.6" />
         </IconBase>
       );
     case "expand":
       return (
         <IconBase className={className}>
-          <path d="M17.5 5v14" />
-          <path d="m8 7.2 4 4.8-4 4.8" />
+          <path d="M17 5v14" />
+          <path d="m8 7.4 4 4.6-4 4.6" />
         </IconBase>
       );
   }
